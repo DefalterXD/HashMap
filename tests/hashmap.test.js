35433fa325bb53eea9a5701597807c0b8b3bf4b2
DefalterXD <103436489+DefalterXD@users.noreply.github.com>
2026-08-12 16:33:44 +0500

    feat: make tests for 'get' method

--- tests/hashmap.test.js
+++ tests/hashmap.test.js
@@ -11,3 +11,25 @@ describe('Return hash code form the key', () => {
         expect(hashMap.hash('0')).toBe(0);
     });
 });
+
+describe('Get value form the key', () => {
+    const hashMap = new HashMap();
+
+    hashMap.set('Alice', 'I am the old value');
+    hashMap.set('0', 'I am the new value');
+    hashMap.set('0', 'I am the value');
+
+
+    
+    test("Return existing value after key", () => {
+        expect(hashMap.get('Alice')).toBe('I am the old value');
+    });
+
+    test("Return existing value after key", () => {
+        expect(hashMap.get('0')).toBe('I am the value');
+    });
+
+    test("Return null value after key", () => {
+        expect(hashMap.get('Wrong')).toBe(null);
+    });
+});
